55777f65dbd9ae9100d16dbc14a472307fd48ba1
YZ318 <YakovZhuravlev88@gmail.com>
2023-05-22 12:19:37 +0300

    Задача 64

---
+++ HomeW64/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void PrintNumbers(int n)
+    {
+        if (n > 0)
+        {
+            Console.WriteLine(n);
+            PrintNumbers(n - 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int n1 = 5;
+        Console.WriteLine("Numbers from {0} to 1:", n1);
+        PrintNumbers(n1);
+
+        int n2 = 8;
+        Console.WriteLine("Numbers from {0} to 1:", n2);
+        PrintNumbers(n2);
+
+        Console.ReadKey();
+    }
+}
