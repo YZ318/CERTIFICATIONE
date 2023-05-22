83d2c61f80ae0b2bac2a802f6e89d91a644f8e07
YZ318 <YakovZhuravlev88@gmail.com>
2023-05-22 12:24:20 +0300

    Задача 66

---
+++ HomeW66/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static int SumNumbers(int m, int n)
+    {
+        if (n == m)
+        {
+            return n;
+        }
+        else
+        {
+            return SumNumbers(m, n - 1) + n;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int m1 = 1, n1 = 15;
+        Console.WriteLine("Sum of numbers from {0} to {1}: {2}", m1, n1, SumNumbers(m1, n1));
+
+        int m2 = 4, n2 = 8;
+        Console.WriteLine("Sum of numbers from {0} to {1}: {2}", m2, n2, SumNumbers(m2, n2));
+
+        Console.ReadKey();
+    }
+}
